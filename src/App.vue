810ef414e6a83ac046dc0bc8cacfa4683d6744dc
SarahLJesus <sarahletjesus1908@gmail.com>
2024-03-20 11:56:52 -0300

    alteração

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,44 @@
 <script setup>
 import { ref } from 'vue'
-const nome = ref('')
-const idade = ref('')
-const email = ref('')
+const user = ref({
+  name: 'Sarah Letícia',
+  surname: 'Jesus',
+  email: 'sarahletjesus1908@gmail.com',
+})
+const mostrarPerfil = ref(false)
 
+function salvarPerfil() {
+  mostrarPerfil.value = true
+}
 </script>
 
 <template>
-  <div class="form">
+  <div class="container">
     <main>
-      <form action="">
-        <h1>Formulaŕio:</h1>
-        <label for="nome">Insira o seu nome:</label>
-        <input v-model="nome" />
-        <p></p>
-        <label for="idade">Insira a sua idade:</label>
-        <input type="number" v-model="idade" />
-        <p></p>
-        <label for="email">Insira o seu email:</label>
-        <input v-model="email" />
-      </form>
+      <h1>Meu Perfil</h1>
+        <transition name="form" mode="out-in">
+        <section v-if="mostrarPerfil">
+        <button class="btn btn-info" @click="mostrarPerfil = false">Esconder</button>
+        </section>
+        <form v-else class="row g-3 was-validated" @submit.prevent="salvarPerfil()" validate>
+          <div class="col-md-4">
+            <label for="nameField" class="form-label">Nome</label>
+            <input type="text" class="form-control" id="nameField" v-model="user.name" required />
+            <div class="invalid-feedback">Nome obrigatório</div>
+          </div>
+          <div class="col-md-4">
+            <label for="surnameField" class="form-label">Sobrenome</label>
+            <input
+              type="text"
+              class="form-control"
+              id="surnameField"
+              v-model="user.surname"
+              required
+            />
+            <div class="invalid-feedback">Sobrenome obrigatório</div>
+          </div>
+          </form>
+        </transition>
     </main>
   </div>
 </template>
